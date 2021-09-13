d5d0abaaaade2fa2a2b5c536eee6558d1cec63e9
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-09-13 15:27:24 -0500

    combination form and saved combos complete

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -3,6 +3,10 @@ export const checkUserEmail = (userEmail) => {
         .then(res => res.json())
 }
 
+export const getCurrentUser = () => {
+    return localStorage.getItem("coburn_customer")
+}
+
 export const createNewUser = (user) => {
     return fetch("http://localhost:8088/users", {
         method: "POST",
@@ -12,4 +16,26 @@ export const createNewUser = (user) => {
         body: JSON.stringify(user)
     })
         .then(res => res.json())
+}
+
+export const getMaterials = () => {
+    return fetch("http://localhost:8088/materials")
+        .then(res => res.json())
+}
+export const getColors = () => {
+    return fetch("http://localhost:8088/colors")
+        .then(res => res.json())
+}
+export const getHinges = () => {
+    return fetch("http://localhost:8088/hinges")
+        .then(res => res.json())
+}
+export const getDimensions = () => {
+    return fetch("http://localhost:8088/dimensions")
+        .then(res => res.json())
+}
+
+export const getAllSavedCombos = () => {
+    return fetch("http://localhost:8088/savedCombinations?_expand=user&_expand=material&_expand=color&_expand=hinge&_expand=dimensions")
+        .then(response => response.json())
 }
--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { Combinations } from './combos/Combinations'
+import { CombinationList} from './combos/CombinationList'
 
 export const ApplicationViews = () => {
     return (
         <>
-            <Route exact path="/build">
+            <Route path="/build">
                 <Combinations />
+            </Route>
+            <Route path="/myCombinations">
+                <CombinationList />
             </Route>
         </>
     )
