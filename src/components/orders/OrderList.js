ea49e7cfcea0e2b1a7166e868d008eb566c15510
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-09-14 14:26:13 -0500

    user can delete combos, and cancel orders

--- src/components/orders/OrderList.js
+++ src/components/orders/OrderList.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { getAllOrders, getCurrentUser } from "../ApiManager"
+import { getAllOrders, getCurrentUser, deleteOrder } from "../ApiManager"
 
 
 
@@ -17,6 +17,16 @@ export const OrderList = () => {
       },
       []
     )
+
+    const cancelOrder = (id) => {
+        deleteOrder(id)
+            .then(() => {
+                getAllOrders()
+                .then((data) => {
+                    setOrders(data)})
+            })
+    }
+
     return (
         <>
             {orders.map(
@@ -27,8 +37,8 @@ export const OrderList = () => {
                             <ul className={`order__list`}>
                                 <li> <h3>{`Order #${comboCounter.length}`} </h3></li>
                                 <button onClick={() => {
-                                    // deleteTicket(ticket.id)
-                                }}>Delete</button>
+                                    cancelOrder(order.id)
+                                }}>Cancel</button>
                             </ul>
                         </div>
                     }
@@ -40,14 +50,3 @@ export const OrderList = () => {
 
 
 
-// {orders.map(
-//     (order) => {
-//     if (parseInt(currentUser) === order.user.Id) {
-//         console.log(orders)
-//         return <div className="orders" key={`order--${order.id}`}>
-//             <ul className="orders__list">
-//                 <li><h3>{`order#: ${order.id}`}</h3></li>
-//             </ul>
-//         </div>
-//     }
-// })}
